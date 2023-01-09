3eeb28478618c5784d672744d2d89564082c64d3
artenlf <felipe.arten@gmail.com>
2023-01-09 11:18:56 -0300

    feat: created header and default layout

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  background-image: URL('../../../public/Cover.png');
+  background-repeat: no-repeat;
+  background-size: 100vw 18.5rem;
+  height: 18.5rem;
+
+  img {
+    margin: auto;
+  }
+`
