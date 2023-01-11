173c9fafe7b7289f504737089b3f26f77c55190e
artenlf <felipe.arten@gmail.com>
2023-01-11 10:11:42 -0300

    feat: added route to Issue page

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,13 +2,14 @@ import { Route, Routes } from 'react-router-dom'
 import { DefaultLayout } from './layouts/DefaultLayout'
 
 import { Home } from './pages/Home'
+import { Issue } from './pages/Issue'
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/post" element={<Checkout />} /> */}
+        <Route path="/issue" element={<Issue />} />
       </Route>
     </Routes>
   )
