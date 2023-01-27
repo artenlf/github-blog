20e03e059d3acc9b2c0ce6b565e0fbb0c209ffd9
artenlf <felipe.arten@gmail.com>
2023-01-27 06:31:08 -0300

    fix: cover import path

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const HeaderContainer = styled.header`
   display: flex;
-  background-image: URL('../../../public/Cover.png');
+  background-image: URL('/Cover.png');
   background-repeat: no-repeat;
   background-size: 100vw 18.5rem;
   height: 18.5rem;
